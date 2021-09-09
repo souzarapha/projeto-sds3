d33457fa20c5bd3514e7240094aeee1d2fa7f767
souzarapha <rapha_souza@outlook.com>
2021-09-09 20:56:43 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DunatChart from "components/DunatCharts";
 
 
 function App() {
@@ -8,9 +10,24 @@ function App() {
     <>
       <NavBar />
       <div className='container'>
-        <h1 className="text-primary"> ola mundo</h1>
+        <h1 className="text-primary py=3" > Dashboard de vendas</h1>
       </div>
-      <DataTable />
+      <div className='row px=2'>
+        <div className='col-sm-6'>
+          <h5 className='text-center text-secundary'>Taxa de sucesso</h5>
+          <BarChart />
+        </div>
+
+        <div className='col-sm-6'>
+          <h5 className='text-center text-secundary'>Todas as vendas</h5>
+          <DunatChart />
+        </div>
+
+        <div className="py=3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+        <DataTable />
+      </div>
       <Footer />
     </>
   );
