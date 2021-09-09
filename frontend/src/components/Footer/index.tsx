c383006b0ebd1274773e0f07205b8f261ad54785
souzarapha <rapha_souza@outlook.com>
2021-09-09 19:50:26 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+
+
+
+function footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/souzarapha" target="_blank" rel="noreferrer">Rafael Souza</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/rafaelsouza.0" target="_blank" rel="noreferrer">@drafaelsouza.0</a></small></p>
+  </div>
+</footer>
+    );
+}
+
+export default footer;
