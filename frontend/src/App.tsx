9ac494679f53e4994387c47964e7ee45068438d0
souzarapha <rapha_souza@outlook.com>
2021-09-12 18:18:27 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,10 @@
-import NavBar from "components/NavBar";
-import Footer from "components/Footer";
-import DataTable from "components/DataTable";
-import BarChart from "components/BarChart";
-import DunatChart from "components/DunatCharts";
+
+import Routes from "Routes";
 
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className='container'>
-        <h1 className="text-primary py=3" > Dashboard de vendas</h1>
-      </div>
-      <div className='row px=2'>
-        <div className='col-sm-6'>
-          <h5 className='text-center text-secundary'>Taxa de sucesso</h5>
-          <BarChart />
-        </div>
-
-        <div className='col-sm-6'>
-          <h5 className='text-center text-secundary'>Todas as vendas</h5>
-          <DunatChart />
-        </div>
-
-        <div className="py=3">
-          <h2 className="text-primary">Todas as vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+    <Routes  />
   );
 }
 
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,35 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DunatChart from "components/DunatCharts";
+
+function Dashboard() {
+    return (
+        <>
+            <NavBar />
+            <div className='container'>
+                <h1 className="text-primary py=3" > Dashboard de vendas</h1>
+            </div>
+            <div className='row px=2'>
+                <div className='col-sm-6'>
+                    <h5 className='text-center text-secundary'>Taxa de sucesso</h5>
+                    <BarChart />
+                </div>
+
+                <div className='col-sm-6'>
+                    <h5 className='text-center text-secundary'>Todas as vendas</h5>
+                    <DunatChart />
+                </div>
+
+                <div className="py=3">
+                    <h2 className="text-primary">Todas as vendas</h2>
+                </div>
+                <DataTable />
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Dashboard;
